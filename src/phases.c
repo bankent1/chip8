b961fe7f7da4e413e13690d6b5cc34d9d9949c55
bankent1 <bankent1@email.arizona.edu>
2019-07-30 22:48:59 -0700

    Started phases

--- src/phases.c
+++ src/phases.c
@@ -1,3 +1,12 @@
+/*
+ * phases.c
+ *
+ * Author: Travis Banken
+ *
+ * Holds the code for the different phases and helper funtions for fetching, 
+ * decoding, and executing instructions.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -20,8 +29,22 @@ int fetch_instr(uint8_t *mem, size_t size, uint16_t addr, uint16_t *instr)
 	return 0;
 }
 
-int decode_instr(uint8_t raw_instr, struct instruction *instr)
+/*
+ * Decodes the given instruction and fills the given instr struct.
+ */
+void decode_instr(uint16_t raw_instr, struct instruction *instr)
 {
-	return 0;
+	instr->opcode = (raw_instr >> 12) & 0xf;
+	instr->vx     = (raw_instr >>  8) & 0xf;
+	instr->vy     = (raw_instr >>  4) & 0xf;
+	instr->funct  = (raw_instr >>  0) & 0xf;
+
+	instr->imm8   = (raw_instr >>  0) & 0xff;
+
+	instr->addr   = (raw_instr >>  0) & 0xfff;
 }
 
+void fill_ctrl_bits(struct instruction *instr, struct crtl_bits *crtl)
+{
+	// TODO: Implement me!
+}
--- src/phases.h
+++ src/phases.h
@@ -1,6 +1,42 @@
+#include <stdint.h>
+
 #ifndef PHASES_H
 #define PHASES_H
 
+// helper struct for holding the different data from the component
+struct instruction {
+	// 4 bits
+	uint8_t opcode;
+	uint8_t vx;
+	uint8_t vy;
+	uint8_t funct;
+	
+	// 8 bits
+	uint8_t imm8;
+
+	// 12 bits
+	uint16_t addr;
+};
+
+// structure for holding the control bits of the emulator
+struct ctrl_bits {
+	uint8_t alu_op;
+	uint8_t bnegate;
+
+	// TODO: Rest of ctrl bits
+};
+
+/*
+ * Fetches the instruction from the given address.
+ *
+ * Returns 1 on bad address call.
+ */
 uint8_t fetch_instr(uint8_t *mem, size_t size, uint16_t addr);
+
+/*
+ * Decodes the given instruction and fills the given instr struct.
+ * Memory for the instruction struct is expected to be alloc before calling.
+ */
+void decode_instr(uint16_t raw_instr, struct instruction *instr);
 
 #endif
